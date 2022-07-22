3ed5a0b4afa9cce995b0938f01b275cbccd9b23a
Steven Carita <stevencaritanina@gmail.com>
2022-07-22 13:56:51 -0300

    Desafio complementario de JS

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Codigo para saber el año de nacimiento
+
+for(let i=0; i>=0; i++){
+    let edad = parseInt(prompt("Ingresa tu edad"))
+    let anioDeNacimiento = alert("Naciste en el año " + (2022 - edad) + " :)");
+}
+
+// Fin del codigo
